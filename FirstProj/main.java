c023b78d57e61fefb0d0be4cbb1bda159924abd3
danial ghaemi <dani77ghaemi@gmail.com>
2019-03-24 23:51:49 +0430

    some changes

--- FirstProj/main.java
+++ FirstProj/main.java
@@ -2,16 +2,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/**
- * Created by M.H.GH.K on 10/5/2018.
- */
 public class main {
     public static void main(String[] args){
         Lexer my=new Lexer();
 
             String data = "";
         try {
-            data = new String(Files.readAllBytes(Paths.get("C:\\Users\\M.H.GH.K\\Desktop\\hes.txt")));
+            data = new String(Files.readAllBytes(Paths.get("C:\\Users\\dani77\\Desktop\\hes.txt")));
         } catch (IOException e) {
             e.printStackTrace();
         }
